e73645d41ea98f0b0f59b52a031dd3fb2a3b4bb5
Adrian Feranu <adrian.feranu@gmail.com>
2020-04-15 16:10:44 +0300

    Updates

---
+++ precache-manifest.54fbfc589592c52fda11ea03b41a64ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "511a7b6d1dfb91dadd77c22ea88d13f5",
+    "url": "/proiect/index.html"
+  },
+  {
+    "revision": "1c09383cebe1c0fd74ce",
+    "url": "/proiect/static/css/main.15fc8f92.chunk.css"
+  },
+  {
+    "revision": "6db1a2a9bd2e9c778822",
+    "url": "/proiect/static/js/2.cf09f371.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/proiect/static/js/2.cf09f371.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c09383cebe1c0fd74ce",
+    "url": "/proiect/static/js/main.62086acc.chunk.js"
+  },
+  {
+    "revision": "b46383e07b18b43e30ae",
+    "url": "/proiect/static/js/runtime-main.ce752e3c.js"
+  }
+]);
